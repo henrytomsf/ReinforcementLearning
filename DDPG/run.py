f9f33f180ce1cb22073ffe3a296daa4a1a2e6acd
Henry Tom <henrytom@Henrys-MacBook-Pro.local>
2020-03-07 21:12:43 -0800

    moved that old code to a new folder

---
+++ DDPG/run.py
@@ -0,0 +1,90 @@
+import numpy as np
+import tensorflow as tf
+import keras.backend as K
+
+from ddpg import DDPG
+from wolpertinger import Wolpertinger
+
+import gym
+
+
+
+def main(env_name='LunarLanderContinuous-v2',
+         low_list=[-1, -1],
+         high_list=[1, 1],
+         pts_list=[1000,1000],
+         b_wolpertinger=True):
+    sess = tf.Session()
+    K.set_session(sess)
+
+    # Define environment
+    # env = gym.make('Pendulum-v0')
+    # env = gym.make('BipedalWalker-v2')
+    env = gym.make(env_name)
+
+    if b_wolpertinger:
+        ddpg = Wolpertinger(env, sess, low_list=low_list, high_list=high_list, points_list=pts_list)
+    else:
+        ddpg = DDPG(env, sess, low_action_bound_list=low_list, high_action_bound_list=high_list)
+
+    # Main loop
+    num_episodes = 200
+    max_episode_len = 1000
+
+    for i in range(num_episodes):
+        total_reward = 0
+
+        current_state = env.reset()
+
+        for step in range(max_episode_len):
+            current_state = current_state.reshape((1, ddpg.state_dim))
+            action = ddpg.act(current_state)
+            if ddpg.action_dim == 1:
+                action = action.reshape((1, ddpg.action_dim))
+            elif ddpg.action_dim > 1:
+                action = action.reshape((1, ddpg.action_dim))[0]
+
+            next_state, reward, done, info = env.step(action)
+            next_state = next_state.reshape((1, ddpg.state_dim))
+            total_reward += reward
+
+            # if step == (max_episode_len-1):
+            #     done = True
+            #     print('Reward: ', total_reward)
+
+            if (step % 5) == 0:
+                ddpg.train()
+                ddpg.update_target_models()
+            
+            ddpg.replay_buffer.add(current_state, action, reward, next_state, done)
+            current_state = next_state
+
+            if done:
+                break
+
+        print('Total Reward for episode {}: {}'.format(i, total_reward))
+        
+        if i == (num_episodes-1):
+            current_state = env.reset()
+            for step in range(500):
+                env.render()
+                current_state = current_state.reshape((1, ddpg.state_dim))
+                action = ddpg.act(current_state)
+                if ddpg.action_dim == 1:
+                    action = action.reshape((1, ddpg.action_dim))
+                elif ddpg.action_dim > 1:
+                    action = action.reshape((1, ddpg.action_dim))[0]
+
+                next_state, reward, done, info = env.step(action)
+                next_state = next_state.reshape((1, ddpg.state_dim))
+
+                current_state = next_state
+                
+                if done:
+                    break
+
+
+
+if __name__ == '__main__':
+    # main(env_name='Pendulum-v0', low_list=[-2], high_list=[2], pts_list=[1000], b_wolpertinger=True)
+    main(env_name='LunarLanderContinuous-v2', low_list=[-1, -1], high_list=[1, 1], pts_list=[1000,1000], b_wolpertinger=False)
---
+++ DDPG/wolpertinger.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pyflann
+from ddpg import DDPG
+import action_space
+
+
+
+class Wolpertinger(DDPG):
+    def __init__(self,
+                 env,
+                 sess,
+                 low_list,
+                 high_list,
+                 points_list, # points in each dimension
+                 k_ratio=0.1):
+        super().__init__(env, sess, low_list, high_list)
+        self.k_ratio = k_ratio
+        if self.continuous_action_space:
+            self.action_space = action_space.Space(low_list, high_list, points_list)
+            self.max_actions = self.action_space.get_num_actions()
+        else:
+            assert len(points_list)==1
+            self.max_actions = points_list[0]
+            self.action_space = action_space.Discrete(self.max_actions)
+        
+        self.knn = max(1, int(self.max_actions * self.k_ratio))
+    
+    def __repr__(self):
+        return 'Wolpertinger_maxactions{}_kratio{}'.format(self.max_actions, self.k_ratio)
+
+    def get_action_space(self):
+        return self.action_space
+
+    def act(self, state):
+        proto_action = super().act(state)
+
+        return self.wolpertinger_action(state, proto_action)
+    
+    def wolpertinger_action(self, state, proto_action):
+        # get the proto action's knn
+        actions = self.action_space.search_point(proto_action, self.knn)[0]
+
+        # create all state action pairs from the search
+        states = np.tile(state, [len(actions), 1])
+
+        # eval each pair from critic
+        actions_eval = self.critic_model.predict([states, actions])
+
+        # find index of action pair with max Q value
+        max_index = np.argmax(actions_eval)
+
+        return actions[max_index]

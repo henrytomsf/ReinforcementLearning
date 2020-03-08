1b71d1facd46e61fcd094c3108da86349c5fc7a5
Henry Tom <henrytom@Henrys-MacBook-Pro.local>
2020-03-07 21:13:23 -0800

    added Twin Delayed DDPG

---
+++ TD3/critic.py
@@ -0,0 +1,54 @@
+import numpy as np
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Input
+from keras.layers.merge import Add, Concatenate
+from keras.optimizers import Adam
+from keras.losses import mse
+import keras.backend as K
+
+import tensorflow as tf
+
+
+
+class Critic:
+    def __init__(self,
+                 state_dim,
+                 action_dim,
+                 learning_rate):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.learning_rate = learning_rate
+
+    def create_critic_model(self):
+        state_input = Input(shape=[self.state_dim])
+        action_input = Input(shape=[self.action_dim])
+
+        # define label layer
+        target_q = Input(shape=[1])
+
+        # first critic
+        state1_h1 = Dense(500, activation='relu')(state_input)
+        state1_h2 = Dense(200, activation='relu')(state1_h1)
+
+        action1_h1 = Dense(500)(action_input)
+
+        merged1= Concatenate()([state1_h2, action1_h1])
+        merged1_h1 = Dense(200, activation='relu')(merged1)
+        output1 = Dense(1, activation='linear')(merged1_h1)
+
+        # second critic
+        state2_h1 = Dense(500, activation='relu')(state_input)
+        state2_h2 = Dense(200, activation='relu')(state2_h1)
+
+        action2_h1 = Dense(500)(action_input)
+
+        merged2 = Concatenate()([state2_h2, action2_h1])
+        merged2_h1 = Dense(200, activation='relu')(merged2)
+        output2 = Dense(1, activation='linear')(merged2_h1)
+
+        model = Model(input=[state_input, action_input, target_q], output=[output1, output2])
+
+        loss = K.mean(mse(output1, target_q) * mse(output2, target_q))
+        model.add_loss(loss)
+
+        return state_input, action_input, model
---
+++ TD3/run.py
@@ -0,0 +1,94 @@
+import numpy as np
+import tensorflow as tf
+import keras.backend as K
+
+from td3 import TD3
+
+import gym
+
+
+
+def main(env_name='LunarLanderContinuous-v2',
+         low_list=[-1, -1],
+         high_list=[1, 1],
+         pts_list=[1000,1000],
+         modeltype='wolpertinger'):
+    sess = tf.Session()
+    K.set_session(sess)
+
+    # Define environment
+    # env = gym.make('Pendulum-v0')
+    # env = gym.make('BipedalWalker-v2')
+    env = gym.make(env_name)
+
+    if modeltype == 'wolpertinger':
+        ddpg = Wolpertinger(env, sess, low_list=low_list, high_list=high_list, points_list=pts_list)
+    elif modeltype == 'ddpg':
+        ddpg = DDPG(env, sess, low_action_bound_list=low_list, high_action_bound_list=high_list)
+    elif modeltype == 'td3':
+        ddpg = TD3(env, sess, low_action_bound_list=low_list, high_action_bound_list=high_list)
+
+    # Main loop
+    num_episodes = 200
+    max_episode_len = 1000
+
+    for i in range(num_episodes):
+        total_reward = 0
+
+        current_state = env.reset()
+
+        for step in range(max_episode_len):
+            current_state = current_state.reshape((1, ddpg.state_dim))
+            action = ddpg.act(current_state)
+            if ddpg.action_dim == 1:
+                action = action.reshape((1, ddpg.action_dim))
+            elif ddpg.action_dim > 1:
+                action = action.reshape((1, ddpg.action_dim))[0]
+
+            next_state, reward, done, info = env.step(action)
+            next_state = next_state.reshape((1, ddpg.state_dim))
+            total_reward += reward
+
+            # if step == (max_episode_len-1):
+            #     done = True
+            #     print('Reward: ', total_reward)
+
+            ddpg.train_critic()
+
+            if (step % 2) == 0:
+                # print('Training actor and updating weights at episode {} step {}'.format(i, step))
+                ddpg.train_actor()
+                ddpg.update_target_models()
+
+            ddpg.replay_buffer.add(current_state, action, reward, next_state, done)
+            current_state = next_state
+
+            if done:
+                break
+
+        print('Total Reward for episode {}: {}'.format(i, total_reward))
+
+        if i == (num_episodes-1):
+            current_state = env.reset()
+            for step in range(500):
+                env.render()
+                current_state = current_state.reshape((1, ddpg.state_dim))
+                action = ddpg.act(current_state)
+                if ddpg.action_dim == 1:
+                    action = action.reshape((1, ddpg.action_dim))
+                elif ddpg.action_dim > 1:
+                    action = action.reshape((1, ddpg.action_dim))[0]
+
+                next_state, reward, done, info = env.step(action)
+                next_state = next_state.reshape((1, ddpg.state_dim))
+
+                current_state = next_state
+
+                if done:
+                    break
+
+
+
+if __name__ == '__main__':
+    # main(env_name='Pendulum-v0', low_list=[-2], high_list=[2], pts_list=[1000], modeltype='td3')
+    main(env_name='LunarLanderContinuous-v2', low_list=[-1, -1], high_list=[1, 1], pts_list=[1000, 1000], modeltype='td3')
